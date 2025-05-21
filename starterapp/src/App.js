4474825b36f3b2adfdb869f5e9814d1ee3b982bb
gounane <gounane.said@gmail.com>
2025-05-21 10:59:40 +0100

    starter code

---
+++ starterapp/src/App.js
@@ -0,0 +1,13 @@
+import Todo from "./components/Todo"
+import "./app.css"
+
+function App(){
+  return (
+    <div className="container">
+      <h1>Tdo List</h1>
+      <Todo/>
+    </div>
+  )
+}
+
+export default App
---
+++ starterapp/src/components/Todo.js
@@ -0,0 +1,10 @@
+function Todo(){
+    return(
+        <div className="todoItem">
+            <p>01</p>
+            <p>Lire React js</p>
+        </div>
+    )
+}
+
+export default Todo
